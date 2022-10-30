3bfa9d1acc7a387e1f4666b49e8d6d2a9b96126b
andress20 <andress20@gmail.com>
2022-10-29 20:11:59 -0500

    Add themes, basic setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,36 +4,16 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Button from '@mui/material/Button'
+import { Typography } from '@mui/material'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Modulop pedidos</title>
-        <meta name="description" content="Modulo de pedidos vulcanos bakery" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          vulcanos bakery <Link href="/about">About</Link>
-        </h1>
-        <Button variant="contained">Pedidos</Button>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <>
+      <Typography variant="h1" color="primary">
+        Vulcano's Bakery
+      </Typography>
+      <Button variant="contained">Pedidos</Button>
+    </>
   )
 }
 
