37a37af82df53e10c382d51ff54656de2f269b7e
andress20 <andress20@gmail.com>
2022-10-29 20:12:43 -0500

    Add ligth and dark basic themes

---
+++ themes/light-theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material'
+import { grey, red } from '@mui/material/colors'
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: grey[50],
+    },
+    primary: {
+      main: red[900],
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  components: {},
+})
